4753f2b77020f22691c1d22b4bcb3b223fdc913a
David <davo00771339@gmail.com>
2023-07-24 19:33:28 +0400

    finished

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import fullpage from 'fullpage.js';
 import './Main.css';
-import verticalPhoneImage from '../assets/verticalPhoneImage.png'
-import horizontalPhoneImage from '../assets/horizontalPhoneImage.png'
+import firstImage from '../assets/firstImage.png'
+import secondImage from '../assets/secondImage.png'
+import thirdImage from '../assets/thirdImage.png'
+import fourthImage from '../assets/fourthImage.png'
+import {BiLogoTelegram} from  'react-icons'
 
 
 const Main = () => {
@@ -23,14 +26,14 @@ const Main = () => {
 
 
     <div className="section home">
-        <div className='logo'></div>
+        <div className='logo' ></div>
         <div className='homeContent'>
-        <h1>Experience the Thrill of Belote, the Card Game Masterpiece!</h1>
-        <p>Belote: Where Strategy Meets Fun</p>
-     <div className='qrSection'>
-          <a href="#"> <div className='qrCode'></div> </a>
+        <h1 className='homeTextHeader'>Experience the Thrill of Belote, the Card Game Masterpiece!</h1>
+        <p className='homeTextParagraph'>Belote: Where Strategy Meets Fun</p>
+     <div className='linksSection'>
+          <a href="https://t.me/BeloteClub" target="_blank" > <div className='telegram'></div> </a>
          <div className='socialMedia'>
-          <a href="#"><div className='appStore'> </div></a>
+          <a href="#" ><div className='appStore'> </div></a>
           <a href="#"> <div className='playMarket'></div></a>
          </div>
      </div>
@@ -41,13 +44,12 @@ const Main = () => {
 
   <div className="section about">
    <div className='aboutContent'>
-     <div className='phoneVertical'> <img className='verticalPhoneImage' src={verticalPhoneImage} alt="Belote" /></div>
+     <div className='phoneVertical'> <img className='verticalPhoneImage' src={firstImage} alt="Belote" /></div>
          <div className='aboutTextContent'>
           <h2 className='aboutTextHeader'>Your ultimate Belote companion, anytime, anywhere!</h2>
-          <p className='aboutTextParagraph'> Discover the captivating world of Belote, where <br></br>strategy meets excitement, and friendships are forged through every<br></br> card played!Immerse yourself in the rich tradition of Belote, where every move counts, and <br></br>the thrill of outsmarting your opponents brings<br></br> unparalleled satisfaction</p>
+          <p className='aboutTextParagraph'> Discover the captivating world of Belote, where <br></br>strategy meets excitement, and friendships are forged through every<br></br> card played!Immerse yourself in the rich tradition of Belote, where every move counts, and <br></br>the thrill of outsmarting your opponents brings.</p>
         </div>
-     </div>
-       
+     </div> 
   </div>
 
 
@@ -58,7 +60,7 @@ const Main = () => {
 
       <div className="section contact">
         <div className='contactContent'>
-        <div className='phoneHorizontal'> <img className='horizontalPhoneImage' src={horizontalPhoneImage}alt="Belote" /></div>
+        <div className='phoneHorizontal'> <img className='horizontalPhoneImage' src={secondImage}alt="Belote" /></div>
         <div className='contactTextContent'>
         <h2 className='contactTextHeader'>Join the Belote community and play like a pro</h2>
         <p className='contactTextParagraph'>Discover the captivating world of Belote, where <br></br>strategy meets excitement, and friendships are forged through every<br></br> card played!Immerse yourself in the rich tradition of Belote, where every move counts, and <br></br>the thrill of outsmarting your opponents brings<br></br> unparalleled satisfaction</p>
@@ -77,9 +79,9 @@ const Main = () => {
         <div className='introContent'>
         <div className='IntroTextContent'>
           <h2 className='IntroTextHeader'>Your ultimate Belote companion, anytime, anywhere!</h2>
-          <p className='IntroTextParagraph'> Discover the captivating world of Belote, where <br></br>strategy meets excitement, and friendships are forged through every<br></br> card played!Immerse yourself in the rich tradition of Belote, where every move counts, and <br></br>the thrill of outsmarting your opponents brings<br></br> unparalleled satisfaction</p>
+          <p className='IntroTextParagraph'> Discover the captivating world of Belote, where <br></br>strategy meets excitement, and friendships are forged through every<br></br> card played!Immerse yourself in the rich tradition of Belote, where every move counts, and <br></br>the thrill of outsmarting your opponents brings.<br></br> unparalleled satisfaction</p>
         </div>
-        <div className='phoneVertical'> <img className='verticalPhoneImage' src={verticalPhoneImage} alt="Belote" /></div>
+        <div className='phoneVertical'> <img className='verticalPhoneImage' src={thirdImage} alt="Belote" /></div>
         </div>
       </div>
 
@@ -95,10 +97,10 @@ const Main = () => {
       
   <div className="section info">
       <div className='infoContent'>
-     <div className='phoneVertical'> <img className='verticalPhoneImage' src={verticalPhoneImage} alt="Belote" /></div>
+     <div className='phoneVertical'> <img className='verticalPhoneImage' src={fourthImage} alt="Belote" /></div>
          <div className='infoTextContent'>
           <h2 className='infoTextHeader'>Your ultimate Belote companion, anytime, anywhere!</h2>
-          <p className='infoTextParagraph'> Discover the captivating world of Belote, where <br></br>strategy meets excitement, and friendships are forged through every<br></br> card played!Immerse yourself in the rich tradition of Belote, where every move counts, and <br></br>the thrill of outsmarting your opponents brings<br></br> unparalleled satisfaction</p>
+          <p className='infoTextParagraph'> Discover the captivating world of Belote, where <br></br>strategy meets excitement, and friendships are forged through every<br></br> card played!Immerse yourself in the rich tradition of Belote, where every move counts, and <br></br>the thrill of outsmarting your opponents brings.</p>
         </div>
      </div>
 
@@ -119,9 +121,9 @@ const Main = () => {
  <div className="section footer">   
     <div className='footerContent'>
         <h1 className='footerHeaderText'>Experience the Thrill of Belote, the Card Game Masterpiece!</h1>
-        <p className='footerParagraphText'>Play Anytime, Anywhere: No need to wait for a card-playing partner or gather a group of friends. Our app allows you<br></br> to enjoy Belote whenever and wherever you want, right from the palm of your hand. <br></br>Challenge your skills against AI opponents or connect with friends for exciting multiplayer battles.</p>
-     <div className='qrSection'>
-          <a href="#"> <div className='qrCode'></div> </a>
+        <p className='footerParagraphText'>Play Anytime, Anywhere: No need to wait for a card-playing partner or gather a group of friends. Our app allows you<br></br> to enjoy Belote whenever and wherever you want,right from the palm of your hand.</p>
+     <div className='linksSection'>
+          <a href="https://t.me/BeloteClub" target="_blank"> <div className='telegram'></div> </a>
          <div className='socialMedia'>
           <a href="#"><div className='appStore'> </div></a>
           <a href="#"> <div className='playMarket'></div></a>
